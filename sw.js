e4eedb60bc4b07c80e4724f6a9ab94c8a4129f32
nayaksofia <nayaksofia1989@gmail.com>
2018-10-07 20:39:57 +0530

    Add fetch event

--- sw.js
+++ sw.js
@@ -18,11 +18,37 @@ const cacheFiles = [
     '/img/9.jpg',
     '/img/10.jpg'
 ];
-
+// Add cacheFiles to the cache
 self.addEventListener('install', function(evt) {
     evt.waitUntil(
         caches.open('v1').then(function(cache) {
             return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+// Fetching 
+self.addEventListener('fetch', function(evt) {
+    evt.respondWith(
+        caches.match(evt.request).then(function(response) {
+            if (response) {
+                console.log('Found ', evt.request, ' in cache');
+                return response;
+            }
+            else {
+                console.log('Could not find ', evt.request, ' in cache, FETCHING!');
+                return fetch(evt.request)
+                .then(function(response) {
+                    const clonedResponse = response.clone(); 
+                    caches.open('v1').then(function(cache) {
+                        cache.put(evt.request, clonedResponse);
+                    })
+                    return response;
+                })
+                .catch(function(err) {
+                    console.error(err);
+                });
+            }
         })
     );
 });
