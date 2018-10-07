aa3cea897291bb439ae3fffccda6abcd4b044732
nayaksofia <nayaksofia1989@gmail.com>
2018-10-07 17:44:16 +0530

    Create an array of files

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheFiles = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
+];
+
