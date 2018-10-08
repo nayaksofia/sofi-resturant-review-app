d590565db2b65a374d45caa2b35024b0e1dc6ad9
nayaksofia <nayaksofia1989@gmail.com>
2018-10-09 01:45:25 +0530

    update the service worker

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 // Register the service worker 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-  .register('/sw.js')
-  .catch(function(err) {
-    console.error(err);
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
   });
-}
-
+} 
 let restaurants,
   neighborhoods,
   cuisines
--- sw.js
+++ sw.js
@@ -1,54 +1,87 @@
-const cacheFiles = [
-    '/',
-    '/index.html',
-    '/restaurant.html',
-    '/css/styles.css',
-    '/js/dbhelper.js',
-    '/js/main.js',
-    '/js/restaurant_info.js',
-    '/data/restaurants.json',
-    '/img/1.jpg',
-    '/img/2.jpg',
-    '/img/3.jpg',
-    '/img/4.jpg',
-    '/img/5.jpg',
-    '/img/6.jpg',
-    '/img/7.jpg',
-    '/img/8.jpg',
-    '/img/9.jpg',
-    '/img/10.jpg'
+//Install a service worker
+
+var CACHE_NAME = 'my-restuarant-cache-v1';
+var urlsToCache = [
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/data/restaurants.json'
+   
 ];
-// Add cacheFiles to the cache
-self.addEventListener('install', function(evt) {
-    evt.waitUntil(
-        caches.open('v1').then(function(cache) {
-            return cache.addAll(cacheFiles);
-        })
-    );
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-// Fetching 
-self.addEventListener('fetch', function(evt) {
-    evt.respondWith(
-        caches.match(evt.request).then(function(response) {
-            if (response) {
-                console.log('Found ', evt.request, ' in cache');
-                return response;
-            }
-            else {
-                console.log('Could not find ', evt.request, ' in cache, FETCHING!');
-                return fetch(evt.request)
-                .then(function(response) {
-                    const clonedResponse = response.clone(); 
-                    caches.open('v1').then(function(cache) {
-                        cache.put(evt.request, clonedResponse);
-                    })
-                    return response;
-                })
-                .catch(function(err) {
-                    console.error(err);
-                });
-            }
+//Activate 
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+  console.log("Service worker activated");
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
+
+//Cache and Return Request 
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          var fetchRequest = event.request.clone();
+  
+          return fetch(fetchRequest).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+
+              var responseToCache = response.clone();
+  
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        })
+      );
+  });
