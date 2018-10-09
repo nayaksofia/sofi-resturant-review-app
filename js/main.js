15872abc275678c186b4b5083c0735071acc5208
nayaksofia <nayaksofia1989@gmail.com>
2018-10-10 00:13:45 +0530

    Update Service Worker Path

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 // Register the service worker 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sofi-resturant-review-app/sw.js').then(function(registration) {
+    // navigator.serviceWorker.register('/sofi-resturant-review-app/sw.js').then(function(registration) {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
