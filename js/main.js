2893ff0818fe8e86b9f84359368db13114c8f570
nayaksofia <nayaksofia1989@gmail.com>
2018-10-09 21:05:53 +0530

    Add path link to sw

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Register the service worker 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('/sofi-resturant-review-app/sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
