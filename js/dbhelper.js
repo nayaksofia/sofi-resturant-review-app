78572903b5c0c3c303ec5035509da88120cb76cb
nayaksofia <nayaksofia1989@gmail.com>
2018-10-09 15:30:14 +0530

    Update Image URL

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -151,7 +151,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`img/${restaurant.photograph}`);
   }
 
   /**
